20ceb6dea38e7fb40b357d18dd5e7a554c520b10
jonschlinkert <jon.schlinkert@sellside.com>
2015-01-02 09:45:14 -0500

    fix copyright date

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /*!
  * fill-range <https://github.com/jonschlinkert/fill-range>
  *
- * Copyright (c) 2014 Jon Schlinkert, contributors.
+ * Copyright (c) 2014-2015, Jon Schlinkert.
  * Licensed under the MIT license.
  */
 
