9f1f644695e6d4679305e5bcd97d651428f9910b
jonschlinkert <jon.schlinkert@sellside.com>
2015-01-02 08:39:43 -0500

    start refactoring

--- index.js
+++ index.js
@@ -7,67 +7,118 @@
 
 'use strict';
 
+var repeat = require('repeat-string');
 var isNumber = require('is-number');
 
-module.exports = function fillRange(a, b, c, fn) {
-  if (typeof c === 'function') {
-    fn = c;
-    c = undefined;
+/**
+ * Expose `fillRange`
+ */
+
+module.exports = fillRange;
+
+
+function fillRange(a, b, increment, fn) {
+  if (typeof increment === 'function') {
+    fn = increment;
+    increment = undefined;
   }
 
-  var bad = /\./.test(a)
-    || /\./.test(b)
-    || c && !isNumber(c)
-    || (!isNumber(Math.abs(a)) && isNumber(Math.abs(b))
-      || isNumber(Math.abs(a)) && !isNumber(Math.abs(b)));
+  var invalid = (/\./.test(a) || /\./.test(b))
+    || (increment && !isNumber(increment))
+    || (!isNumber(Math.abs(a)) && isNumber(Math.abs(b)))
+    || (isNumber(Math.abs(a)) && !isNumber(Math.abs(b)));
 
-  if (bad) {
-    return {bad: [ '{' + [a, b, c].filter(Boolean).join('..') + '}']};
+  if (invalid) {
+    return {invalid: [a, b, increment].filter(Boolean).join('..')};
   }
 
-  var inc = typeof c !== 'undefined'
-      ? (+c < 0) ? (+c * -1) : +c
-      : 1;
+  var astr = a.toString();
+  var bstr = b.toString();
+  var padding;
 
-  var isLetter = !isNumber(+a);
+  // Was an increment (step) passed?
+  increment = typeof increment !== 'undefined'
+    ? Math.abs(increment)
+    : 1;
+
+  // is the range alphabetical? or numeric?
+  var isLetter = !isNumber(a);
   a = isLetter ? a.charCodeAt(0) : +a;
   b = isLetter ? b.charCodeAt(0) : +b;
 
-  if (b < a) {
-    return negativeRange(a, b, inc, fn, isLetter);
+  if (!isLetter) {
+    padding = isPadded(a, b);
   }
 
-  return positiveRange(a, b, inc, fn, isLetter);
-};
 
-function positiveRange(a, b, inc, fn, isLetter) {
-  var arr = [];
-  a -= inc;
+  function positiveRange(a, b, increment, fn, isLetter) {
+    var arr = [];
+    var pad = '';
+    var i = 0;
+    var res;
 
-  for (var i = 0; a < b; i++) {
-    a += inc;
-    if (a <= b) {
-      var res = fn && fn(a, isLetter, i);
-      arr.push(res ? res : isLetter
-        ? String.fromCharCode(a)
-        : String(a));
+    while (a <= b) {
+      // var pad = !isLetter ? String(repeat('0', padding)) : '';
+      // if (padding && !res && !isLetter) {
+      //   a = pad + a;
+      // }
+
+      if (typeof fn === 'function') {
+        res = fn(a, isLetter, pad, i++);
+      } else if (isLetter) {
+        res = String.fromCharCode(a);
+      } else {
+        res = String(a);
+      }
+
+      arr.push(res);
+      a += increment;
     }
+    return arr;
   }
-  return arr;
-}
 
-function negativeRange(a, b, inc, fn, isLetter) {
-  var arr = [];
-  a += inc;
+  function negativeRange(a, b, increment, fn, isLetter) {
+    var arr = [];
+    var pad = '';
+    var i = 0;
+    var res;
 
-  for (var i = 0; a > b; i--) {
-    a -= inc;
-    if (a >= b) {
-      var res = fn && fn(a, isLetter, i);
-      arr.push(res ? res : isLetter
-        ? String.fromCharCode(a)
-        : String(a));
+    while (a >= b) {
+      // var pad = !isLetter ? String(repeat('0', padding)) : '';
+      // if (padding && !res && !isLetter) {
+      //   a = pad + a;
+      // }
+
+      if (typeof fn === 'function') {
+        res = fn(a, isLetter, pad, i++);
+      } else if (isLetter) {
+        res = String.fromCharCode(a);
+      } else {
+        res = String(a);
+      }
+
+      arr.push(res);
+      a -= increment;
     }
+    return arr;
   }
-  return arr;
+
+  function isPadded(a, b, isLetter) {
+    var res = /^-*(0*)[0-9]/.exec(astr);
+    var len = res && res[1].length;
+    if (!!(res && len > 0)) {
+      return len;
+    }
+
+    return false;
+  }
+
+  function expand(a, b, increment, fn) {
+    if (b < a) {
+      return negativeRange(a, b, increment, fn, isLetter, padding);
+    }
+    return positiveRange(a, b, increment, fn, isLetter, padding);
+  }
+
+  return expand(a, b, increment, fn)
 }
