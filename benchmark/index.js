ba5c5b4c5abaa40692fa6ba108a9fd57c410aa1e
jonschlinkert <jon.schlinkert@sellside.com>
2016-09-10 21:03:37 -0400

    run update, refactor benchmarks

--- benchmark/index.js
+++ benchmark/index.js
@@ -1,11 +1,33 @@
 'use strict';
 
+var path = require('path');
+var util = require('util');
+var cyan = require('ansi-cyan');
+var argv = require('yargs-parser')(process.argv.slice(2));
 var Suite = require('benchmarked');
-var suite = new Suite({
-  result: false,
-  fixtures: 'fixtures/*.js',
-  add: 'code/*.js',
-  cwd: __dirname
-});
 
-suite.run();
+function run(type, fixtures) {
+  var suite = new Suite({
+    cwd: __dirname,
+    fixtures: `fixtures/${fixtures}.js`,
+    code: `code/${type}*.js`
+  });
+
+  if (argv.dry) {
+    suite.dryRun(function(code, fixture) {
+      console.log(arguments)
+      console.log(cyan('%s > %s'), code.key, fixture.key);
+      var args = require(fixture.path);
+      var res = code.run(args);
+      if (Array.isArray(res)) {
+        res = res.length;
+      }
+      console.log(util.inspect(res, null, 10));
+      console.log();
+    });
+  } else {
+    suite.run();
+  }
+}
+
+run(argv._[0] || '*');
